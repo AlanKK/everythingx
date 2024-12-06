3fedd4b25eafc647f410068952089b07efc6d255
Alan Keister <alankeister@gmail.com>
2024-12-06 12:20:44 -0500

    General cleanup

---
+++ db.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var records []struct {
+	filename string
+	path     string
+}
+
+func openDB(pathname string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", pathname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("PRAGMA case_sensitive_like = ON")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db, err
+}
+
+func createDBAndTable(pathname string) (*sql.DB, error) {
+	db, err := openDB(pathname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS files (filename TEXT, fullpath TEXT)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("CREATE INDEX idx_filename ON files(filename COLLATE BINARY)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db, err
+}
+
+func prefixSearch(db *sql.DB, prefix string, limit ...int) ([]string, error) {
+	var results []string
+
+	resultLimit := 20
+	if len(limit) > 0 {
+		resultLimit = limit[0]
+	}
+
+	rows, err := db.Query(
+		"SELECT filename, fullpath FROM files WHERE filename LIKE ? COLLATE BINARY ORDER BY filename ASC LIMIT ?",
+		prefix+"%", resultLimit)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var filename, fullpath string
+		err := rows.Scan(&filename, &fullpath)
+		if err != nil {
+			return nil, err
+		}
+		fmt.Printf("Filename: %s, Fullpath: %s\n", filename, fullpath)
+
+		results = append(results, fullpath)
+	}
+
+	return results, nil
+}
+
+func deleteRecord(db *sql.DB, key string) error {
+	_, err := db.Exec("DELETE FROM files WHERE filename = ?", key)
+	return err
+}
+
+func insertRecord(db *sql.DB, filename string, path string) error {
+	_, err := db.Exec("INSERT INTO files (filename, fullpath) VALUES (?, ?)", filename, path)
+	return err
+}
+
+func bulkInsertRecords(db *sql.DB, filename string, path string) error {
+	records = append(records, struct {
+		filename string
+		path     string
+	}{filename, path})
+
+	if len(records) >= 10000 {
+		tx, err := db.Begin()
+		if err != nil {
+			return err
+		}
+
+		stmt, err := tx.Prepare("INSERT INTO files (filename, fullpath) VALUES (?, ?)")
+		if err != nil {
+			return err
+		}
+		defer stmt.Close()
+
+		for _, record := range records {
+			_, err = stmt.Exec(record.filename, record.path)
+			if err != nil {
+				tx.Rollback()
+				return err
+			}
+		}
+
+		err = tx.Commit()
+		if err != nil {
+			return err
+		}
+
+		records = records[:0] // Clear the slice
+	}
+
+	return nil
+}
+
+func commitRecords(db *sql.DB) error {
+	if len(records) == 0 {
+		return nil
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.Prepare("INSERT INTO files (filename, fullpath) VALUES (?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	for _, record := range records {
+		_, err = stmt.Exec(record.filename, record.path)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	records = records[:0] // Clear the slice
+	return nil
+}
--- ui.go
+++ ui.go
@@ -1,47 +1,74 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"os/exec"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 
-	widgetx "fyne.io/x/fyne/widget"
+	widgetx "github.com/matwachich/fynex-widgets"
 )
 
-func loadUI() {
+func handleOpenButtonClick(pathname string) {
+	fmt.Println("handleOpenButtonClick: ", pathname)
+
+	cmd := exec.Command("open", "-R", pathname)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
+
+func handleEntryChanged(db *sql.DB, entry *widgetx.AutoComplete) {
+	if len(entry.Text) == 0 {
+		entry.ListHide()
+		return
+	}
+	// Calling prefixSearch without a limit parameter, using the default limit of 20
+	results, err := prefixSearch(db, entry.Text, 200)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Results:", len(results))
+
+	// no results
+	if len(results) == 0 {
+		if entry.Text == "" {
+			return
+		}
+	}
+
+	// then show them
+	entry.Options = results
+	entry.ListShow()
+	printMemUsage()
+}
+
+func loadUI2(db *sql.DB) {
 	a := app.New()
 	w := a.NewWindow("Entry Completion")
 
-	input := widgetx.NewCompletionEntry([]string{})
-	input.SetPlaceHolder("Enter filename...")
+	entry := widgetx.NewAutoComplete(1)
+	entry.SetPlaceHolder("Enter filename...")
 
 	content := container.NewVBox(
-		input,
-		widget.NewButton("Open", func() {
-			fmt.Println("Text:", input.Text)
+		entry,
+		widget.NewButton("Show in Finder", func() {
+			handleOpenButtonClick(entry.Text)
 		}),
 	)
-	input.OnChanged = func(s string) {
-		results := getFileList(input.Text)
-		fmt.Println("Results:", len(results))
-
-		// no results
-		if len(results) == 0 {
-			input.HideCompletion()
-			return
-		}
-
-		// then show them
-		input.SetOptions(results)
-		input.ShowCompletion()
+	entry.OnChanged = func(s string) {
+		handleEntryChanged(db, entry)
 	}
 
 	w.SetContent(content)
 
-	w.Resize(fyne.NewSize(600, 500))
+	w.Resize(fyne.NewSize(1300, 800))
 	w.ShowAndRun()
 
 	// anything below will not be executed until app is closed
