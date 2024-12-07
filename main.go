cf7dee05559920e54fec5f884519d0360725c656
Alan Keister <alankeister@gmail.com>
2024-12-06 19:19:09 -0500

    Good evening

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func main() {
 	}
 	defer db.Close()
 
-	loadUI2(db)
+	loadUI(db)
 }
 
 func loadDBFromFile(db *sql.DB, filename string) error {
@@ -79,33 +79,3 @@ func loadDBFromFile(db *sql.DB, filename string) error {
 	fmt.Println("Loaded", records, "records from %s.", filename)
 	return nil
 }
-
-// import (
-// 	"fmt"
-
-// 	"fyne.io/fyne/v2"
-// 	"fyne.io/fyne/v2/app"
-// 	"fyne.io/fyne/v2/container"
-// 	"fyne.io/fyne/v2/widget"
-// )
-// func main() {
-// 	myApp := app.New()
-// 	myWindow := myApp.NewWindow("Text Box Example")
-
-// 	input := widget.NewEntry()
-// 	input.SetPlaceHolder("Enter text here...")
-
-// 	content := container.NewVBox(
-// 		widget.NewLabel("Enter some text:"),
-// 		input,
-// 		widget.NewButton("Submit", func() {
-// 			fmt.Println("Text entered:", input.Text)
-// 		}),
-// 	)
-
-// 	myWindow.SetContent(content)
-// 	myWindow.Resize(fyne.NewSize(300, 200))
-// 	myWindow.Show()
-// 	myApp.Run()
-// 	fmt.Println("Done.")
-// }
