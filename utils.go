79c0c3a3ffee01230d521f96ca298d0f198d97b9
Alan Keister <alankeister@gmail.com>
2024-12-13 10:46:58 -0500

    status bar updates, code reorg

---
+++ utils.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"time"
+)
+
+func getFileInfo(path string) (int64, time.Time, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return 0, time.Time{}, err
+	}
+
+	size := fileInfo.Size()
+	lastModified := fileInfo.ModTime()
+
+	return size, lastModified, nil
+}
+
+func printFileInfo(path string) {
+	size, lastModified, err := getFileInfo(path)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Printf("File size: %d bytes\n", size)
+	fmt.Printf("Last modified: %s\n", lastModified)
+}
+
+func printMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("Alloc = %v MB", bToMb(m.Alloc))
+}
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
