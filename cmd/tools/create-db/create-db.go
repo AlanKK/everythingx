d249cd7ff30bb1c3fdfd37c0a8cbd3ab58e759e3
Alan Keister <alankeister@gmail.com>
2025-01-11 09:45:06 -0500

    Refactor database setup and command-line argument handling; add signal handlers for graceful shutdown and disk scanning

--- cmd/tools/create-db/create-db.go
+++ cmd/tools/create-db/create-db.go
@@ -11,10 +11,11 @@ func main() {
 	pathname := flag.String("path", "/Users/alan/Documents/findfiles/files.db", "Path to the database file")
 	flag.Parse()
 
-	err := ffdb.CreateDBAndTable(*pathname)
+	db, err := ffdb.CreateDB(*pathname)
 	if err != nil {
 		log.Fatalf("Expected no error, got %v", err)
 	}
+	db.Close()
 
 	log.Println("Database created: ", *pathname)
 }
