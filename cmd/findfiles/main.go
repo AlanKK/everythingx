912877f3b0e1f8deb87e6ba04371a5d11e435bba
Alan Keister <alankeister@gmail.com>
2025-01-14 16:45:50 -0500

    Got installation and uninstallation scripts working; create Makefile and plist for service management.  Fix up UI.

--- cmd/findfiles/main.go
+++ cmd/findfiles/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"database/sql"
+	"flag"
 	"fmt"
 	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/AlanKK/findfiles/internal/ffdb"
 )
@@ -16,62 +13,67 @@ func fileExists(filename string) bool {
 	return !os.IsNotExist(err)
 }
 
-var dataFolder = "../../data"
-
 func main() {
-	dbFile := dataFolder + "/files.db"
+	dbFile := getCommandLineArgs()
 
 	if !fileExists(dbFile) {
-		fmt.Println("Database does not exist. Creating it.")
-		db, err := ffdb.CreateDB(dbFile)
-		if err != nil {
-			return
-		}
-		defer db.Close()
-
-		err = loadDBFromFile(db, dataFolder+"/all-files.txt")
-		if err != nil {
-			return
-		}
+		fmt.Println("Database does not exist ", dbFile)
+		os.Exit(1)
 	}
 
-	fmt.Println("Opened database ", dbFile)
+	fmt.Println("Opening database ", dbFile)
+	_, err := ffdb.OpenDB(dbFile)
+	if err != nil {
+		fmt.Println("Error opening database: ", dbFile, err)
+		os.Exit(1)
+	}
 
 	loadUI()
 }
 
-func loadDBFromFile(db *sql.DB, filename string) error {
-	var records int = 0
+func getCommandLineArgs() string {
+	dbFile := flag.String("db_path", "/var/lib/findfiles/files.db", "Path to the database file")
+	flag.Parse()
 
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return err
+	if *dbFile == "" {
+		fmt.Println("Database path is required")
+		os.Exit(1)
 	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if line == "" || line == "/" {
-			continue
-		}
-		base := filepath.Base(line)
-		err = ffdb.BulkInsertRecords(db, base, line)
-		if err != nil {
-			fmt.Println("Error inserting record:", err)
-			return err
-		}
-
-		records++
-	}
-
-	err = ffdb.CommitRecords(db)
-	if err != nil {
-		fmt.Println("Error committing records:", err)
-		return err
-	}
-
-	fmt.Println("Loaded", records, "records from %s.", filename)
-	return nil
+	return *dbFile
 }
+
+// func loadDBFromFile(db *sql.DB, filename string) error {
+// 	var records int = 0
+
+// 	file, err := os.Open(filename)
+// 	if err != nil {
+// 		fmt.Println("Error opening file:", err)
+// 		return err
+// 	}
+// 	defer file.Close()
+
+// 	scanner := bufio.NewScanner(file)
+// 	for scanner.Scan() {
+// 		line := strings.TrimSpace(scanner.Text())
+// 		if line == "" || line == "/" {
+// 			continue
+// 		}
+// 		base := filepath.Base(line)
+// 		err = ffdb.BulkInsertRecords(db, base, line)
+// 		if err != nil {
+// 			fmt.Println("Error inserting record:", err)
+// 			return err
+// 		}
+
+// 		records++
+// 	}
+
+// 	err = ffdb.CommitRecords(db)
+// 	if err != nil {
+// 		fmt.Println("Error committing records:", err)
+// 		return err
+// 	}
+
+// 	fmt.Println("Loaded", records, "records from %s.", filename)
+// 	return nil
+// }
