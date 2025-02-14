929691ce45a202d96015913708d32e8060cc3a63
Alan KK <AlanKK@users.noreply.github.com>
2025-02-14 10:06:36 -0500

    Added command-line argument "-name searchterm" for terminal "find" functionality

--- cmd/everythingx/main.go
+++ cmd/everythingx/main.go
@@ -14,7 +14,7 @@ func fileExists(filename string) bool {
 }
 
 func main() {
-	dbFile := getCommandLineArgs()
+	dbFile, searchTerm := getCommandLineArgs()
 
 	if !fileExists(dbFile) {
 		fmt.Println("Database does not exist ", dbFile)
@@ -28,16 +28,30 @@ func main() {
 		os.Exit(1)
 	}
 
+	if searchTerm != "" {
+		results, err := ffdb.PrefixSearch(searchTerm, 1000)
+		if err != nil {
+			fmt.Println("Error searching for ", searchTerm, err)
+			os.Exit(1)
+		}
+		for _, r := range results {
+			before, term, after := splitFileName(r.Fullpath, searchTerm)
+			fmt.Printf("%s\033[1m%s\033[0m%s\n", before, term, after)
+		}
+		os.Exit(0)
+	}
+
 	loadUI()
 }
 
-func getCommandLineArgs() string {
+func getCommandLineArgs() (string, string) {
 	dbFile := flag.String("db_path", "/var/lib/findfiles/files.db", "Path to the database file")
+	searchTerm := flag.String("name", "", "Search term")
 	flag.Parse()
 
 	if *dbFile == "" {
 		fmt.Println("Database path is required")
 		os.Exit(1)
 	}
-	return *dbFile
+	return *dbFile, *searchTerm
 }
