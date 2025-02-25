3bfdb2452f108935f8fab72627b7715b2d9996e7
AlanKK <AlanKK@users.noreply.github.com>
2025-02-25 09:49:37 -0500

    Install app, rename com.example to com.github.alankk (#10)

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+# set -x
 
 # Check if the script is being run as root
 if [ "$(id -u)" -ne 0 ]; then
@@ -11,24 +11,26 @@ fi
 echo 'Installing executables'
 cp -f bin/everythingxd /usr/local/bin
 chmod +x /usr/local/bin/everythingxd
-cp -f bin/everythingx /usr/local/bin
 cp -f bin/ev /usr/local/bin
-chmod +x /usr/local/bin/everythingx /usr/local/bin/ev
+chmod +x /usr/local/bin/ev
+
+cp -rf EverythingX.app /Applications
+chmod +x /Applications/EverythingX.app
 
 echo 'Creating data directory /var/lib/everythingx'
 mkdir /var/lib/everythingx
 
 echo 'Installing launchd service'
-cp -f /Users/alan/Documents/git/everythingx/cmd/service/com.example.everythingxd.plist /Library/LaunchDaemons/com.example.everythingxd.plist
-chmod 644 /Library/LaunchDaemons/com.example.everythingxd.plist
-chown root:wheel /Library/LaunchDaemons/com.example.everythingxd.plist
+cp -f /Users/alan/Documents/git/everythingx/cmd/service/com.github.alankk.everythingxd.plist /Library/LaunchDaemons/com.github.alankk.everythingxd.plist
+chmod 644 /Library/LaunchDaemons/com.github.alankk.everythingxd.plist
+chown root:wheel /Library/LaunchDaemons/com.github.alankk.everythingxd.plist
 
 echo 'Registering and starting service with launchd'
-launchctl bootout   system /Library/LaunchDaemons/com.example.everythingxd.plist > /dev/null 2>&1
-launchctl bootstrap system /Library/LaunchDaemons/com.example.everythingxd.plist
-launchctl start com.example.everythingx
+launchctl bootout   system /Library/LaunchDaemons/com.github.alankk.everythingxd.plist > /dev/null 2>&1
+launchctl bootstrap system /Library/LaunchDaemons/com.github.alankk.everythingxd.plist
+launchctl start com.github.alankk.everythingx
 
 echo 'Installation complete. See /var/log/everythingx.log for logs'
 
 # Unload service
-#launchctl bootout system /Library/LaunchDaemons/com.example.everythingxd.plist
+#launchctl bootout system /Library/LaunchDaemons/com.github.alankk.everythingxd.plist
